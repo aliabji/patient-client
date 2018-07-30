6ea7e2cf383677be54652b19e3cf9f961e157b3c
aliabji <aliabji@gmail.com>
2018-07-29 21:40:00 -0400

    performing and displaying landing page api call, detailed patient + encounter call

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+
 class Home extends React.Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            patients: null
+            patients: []
         }
     }
 
     componentDidMount(props) {
-        fetch('http://localhost:3000/patients').then((response) => {
-            console.log(response)
+        fetch("http://localhost:3000/patients").then((data) => {
+            return data.json()
+        }).then((data) => {
+            this.setState({
+                patients: data.data
+            })
         })
+    }
+
+    showState = () => {
+        console.log(this.state.patients)
     }
 
     render() {
@@ -22,7 +31,12 @@ class Home extends React.Component {
                 <header className="home-header">
                 </header>
                 <p>Home</p>
-                <Link to={`/patient`}>Patient Details</Link>
+                <button onClick={this.showState}>Show state</button>
+                <ul>
+                    {this.state.patients.map(p => <li key={p.id}>{p.first}, {p.last}, {p.MRN} 
+                    <Link to={{pathname: `/patient`, state: {id: p.id}}}>Patient Details</Link></li>)}
+                </ul>
+                
             </div>
         )
     }
--- src/Components/patient.jsx
+++ src/Components/patient.jsx
@@ -2,12 +2,46 @@ import React from 'react';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const Patient = () => (
-	<div className="home">
-    	<header className="home-header">
-    	</header>
-        <p>Patient</p>
-  	</div>
-);
+class Patient extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            patient: {}
+        }
+        
+    }
+
+    componentDidMount(props) {
+        console.log(this.props.location.state)
+        console.log(`http://localhost:3000/patients/${this.props.location.state.id}`)
+        fetch(`http://localhost:3000/patients/${this.props.location.state.id}`).then((data) => {
+            return data.json()
+        }).then((data) => {
+            console.log(data)
+            this.setState({
+                patient: data.patient,
+                encounters: data.encounter
+            })
+        })
+    }
+
+    showState = () => {
+        console.log(this.state.patient, this.state.encounters)
+    }
+
+    render() {
+        return(
+            <div className="home">
+                <header className="home-header">
+                </header>
+                <p>Patient</p>
+                <button onClick={this.showState}>Show state</button>
+            </div>
+        )
+    }
+
+}
+
 
 export default Patient;
